655755143b82fd3cad8e31ef979c689e6d88f33f
Mario Camelo <mariuo@gmail.com>
2021-01-27 10:12:25 -0300

    Create product listing

--- front-web/src/core/pages/Catalog/index.tsx
+++ front-web/src/core/pages/Catalog/index.tsx
@@ -1,6 +1,19 @@
+import ProductCard from './components/ProductCard';
+import './styles.scss';
+
 const Catalog = () => (
-    <div>
-        <h1> Catalog</h1>
+    <div className="catalog-container">
+        <h1 className="catalog-title"> Catálogo de Produtos</h1>
+        <div className="catalog-products">
+           <ProductCard />
+           <ProductCard />
+           <ProductCard />
+           <ProductCard />
+           <ProductCard />
+ <ProductCard />
+ <ProductCard />
+ <ProductCard />
+        </div>
     </div>
 );
 
