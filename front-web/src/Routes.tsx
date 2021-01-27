03f6f30056a1f142e613f37e6d6e8dc4e0a228b7
Mario Camelo <mariuo@gmail.com>
2021-01-27 11:32:52 -0300

    Created Product Details layout

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -3,23 +3,27 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './core/components/Navbar';
 import Admin from './core/pages/Admin';
 import Catalog from './core/pages/Catalog';
+import ProductDetails from './core/pages/Catalog/components/ProductDetails';
 import Home from './core/pages/Home';
 
 
 const Routes = () => (
     <BrowserRouter>
-    <Navbar />
-    <Switch>
-        <Route path="/" exact>
-        <Home />
-     </Route>
-     <Route path="/catalog">
-         <Catalog />
-     </Route>
-     <Route path="/admin">
-         <Admin />
-     </Route>
-    </Switch>    
+        <Navbar />
+        <Switch>
+            <Route path="/" exact>
+                <Home />
+            </Route>
+            <Route path="/products" exact>
+                <Catalog />
+            </Route>
+            <Route path="/products/:productId">
+                <ProductDetails />
+            </Route>
+            <Route path="/admin">
+                <Admin />
+            </Route>
+        </Switch>
     </BrowserRouter>
 
 );
--- front-web/src/core/pages/Catalog/components/ProductCard/index.tsx
+++ front-web/src/core/pages/Catalog/components/ProductCard/index.tsx
@@ -1,5 +1,6 @@
 import './styles.scss';
 import { ReactComponent as ProductImage } from '../../../../assets/images/product.svg'
+import ProductPrice from '../../../../components/ProductPrice';
 const ProductCard = () => (
     <div className="card-base border-radius-10 product-card">
         <ProductImage />
@@ -7,10 +8,7 @@ const ProductCard = () => (
             <h6 className="product-name">
                 Computador Desktop - Intel Core i7
             </h6>
-            <div className="product-price-container">
-                <span className="product-currency">R$</span>
-                <h3 className="product-price">2.779,00</h3>
-            </div>
+            <ProductPrice price="2.779,00"/>
         </div>
     </div>
 );
