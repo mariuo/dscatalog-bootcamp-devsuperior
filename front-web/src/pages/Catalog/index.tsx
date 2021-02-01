84f34d48336752c841ebbca865c71d0756526eab
Mario Camelo <mariuo@gmail.com>
2021-02-01 11:26:42 -0300

    Created core/pagination dynamic

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ProductCard from './components/ProductCard';
-import './styles.scss';
 import { makeRequest } from 'core/utils/request';
 import { ProductsResponse } from 'core/types/Product';
 import ProductCardLoader from './components/Loaders/ProductCardLoader';
+import Pagination from 'core/components/Pagination';
+import './styles.scss';
 
 
 const Catalog = () => {    
 
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
     const [isLoading, setIsLoading]= useState(false);
+    const [activePage, setActivePage] = useState(0);
 
     useEffect( () => {
         const params = {
-            page:0,
+            page:activePage,
             linesPerPage: 12
         }
         setIsLoading(true);
@@ -23,7 +25,7 @@ const Catalog = () => {
         .finally(() => {
             setIsLoading(false);
         })
-    }, [])
+    }, [activePage]);
 
     return (
     <div className="catalog-container">
@@ -39,6 +41,13 @@ const Catalog = () => {
             ))
            )}
         </div>
+        {productsResponse && (
+        <Pagination 
+        totalPages={productsResponse?.totalPages}
+        activePage={activePage}
+        onChange={page => setActivePage(page)}
+        />
+        )}
     </div>
     )
 };
