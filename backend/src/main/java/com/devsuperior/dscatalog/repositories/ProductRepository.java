51242984029246b95cc4bbaf6bb5d7f6db873a63
Mario Camelo <mariuo@gmail.com>
2021-02-12 12:19:08 -0300

    First upload test AWS S3

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
@@ -15,7 +15,7 @@ import com.devsuperior.dscatalog.entities.Product;
 public interface ProductRepository extends JpaRepository<Product, Long>{
 	
 	@Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE "
-			+ "(COALESCE(:categories) IS NULL OR :cats IN :categories) AND "
+			+ "(COALESCE(:categories) IS NULL OR cats IN :categories) AND "
 			+ "(LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%')))")
 	Page<Product> find(List<Category> categories, String name, Pageable pageable);
 
