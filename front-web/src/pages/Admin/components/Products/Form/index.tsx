2ec2a001f556ed31b02a0b9ab48f144cec268a1d
Mario Camelo <mariuo@gmail.com>
2021-03-08 10:47:01 -0300

    Add categories on product creation

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -3,27 +3,34 @@ import { toast } from 'react-toastify';
 import { makePrivateRequest, makeRequest } from "core/utils/request";
 import BaseForm from "../../BaseForm";
 import './styles.scss';
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { useHistory, useParams } from "react-router-dom";
+import Select from 'react-select';
+import { Category } from "core/types/Product";
 
 type FormState = {
     name: string;
     price: string;
     description: string;
     imgUrl: string;
+    categories: Category[];
 }
 
 type ParamsType = {
     productId: string;
 }
 
+
 const Form = () => {
-    const { register, handleSubmit, errors, setValue } = useForm<FormState>();
+    const { register, handleSubmit, errors, setValue, control } = useForm<FormState>();
     const history = useHistory();
     const { productId } = useParams<ParamsType>();
+    const [isLoadingCategories, setIsLoadingCategories] = useState(false);
+    const [categories, setCategories] = useState<Category[]>([]);
     const isEditing = productId !== 'create';
     const formTitle = isEditing ? 'Editar Produto': "Cadastrar um Produto";
 
+   
     useEffect(() => {
        if (isEditing){
         makeRequest({ url: `/products/${productId}` })
@@ -32,9 +39,17 @@ const Form = () => {
             setValue('price', response.data.price);
             setValue('description', response.data.description);
             setValue('imgUrl', response.data.imgUrl);
+            setValue('categories', response.data.categories)
         })
         }
     }, [productId, isEditing, setValue]);
+
+    useEffect(()=>{
+        setIsLoadingCategories(true);
+        makeRequest({url: '/categories'})
+        .then(response => setCategories(response.data.content))
+        .finally(()=> setIsLoadingCategories(false));
+    },[]);
 
     const onSubmit = (data: FormState) => {
         makePrivateRequest({ 
@@ -77,6 +92,27 @@ const Form = () => {
                                 </div>
                             )}
                         </div>
+                        <div className="margin-bottom-30">
+                            <Controller
+                            name="categories"
+                            rules={{ required:true}}
+                            control={control}
+                            as={Select} 
+                            options={categories}
+                            isLoading={isLoadingCategories}
+                            getOptionLabel={(option: Category)=> option.name}
+                            getOptionValue={(option: Category)=> String(option.id)}                            
+                            classNamePrefix="categories-select"
+                            placeholder="Categorias"
+                            isMulti
+                        />
+                            {errors.categories && (
+                                <div className="invalid-feedback d-block">
+                                    Campo obrigatório
+                                </div>
+                            )}
+                        </div>            
+
                         <div className="margin-bottom-30">
                             <input
                                 ref={register({ required: "Campo obrigatório" })}
--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -6,7 +6,7 @@ import { makePrivateRequest, makeRequest } from 'core/utils/request';
 import { ProductsResponse } from 'core/types/Product';
 import Pagination from 'core/components/Pagination';
 import { toast } from 'react-toastify';
-
+import CardLoader from '../Loaders/ProductCardLoader';
 const List = () => {
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
     const [isLoading, setIsLoading] = useState(false);
@@ -55,9 +55,12 @@ const List = () => {
                 ADICIONAR
             </button>
             <div className="admin-list-container">
-                {productsResponse?.content.map(product => (
-                    <Card product={product} key={product.id} onRemove={onRemove} />
-                ))}
+                {isLoading ? <CardLoader /> : (
+                       productsResponse?.content.map(product => (
+                            <Card product={product} key={product.id} onRemove={onRemove} />
+                        ))
+                )}
+                
                 {productsResponse && (
                     <Pagination
                         totalPages={productsResponse?.totalPages}
