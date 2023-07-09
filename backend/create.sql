fc3dc823985b429e71545b6a833ffbde01b298e6
Mario Camelo <mariuo@gmail.com>
2023-07-09 18:21:43 -0400

    :sparkles: fix: Files from github

--- backend/create.sql
+++ backend/create.sql
@@ -9,3 +9,4 @@ alter table if exists tb_product_category add constraint FK5r4sbavb4nkd9xpl0f095
 alter table if exists tb_product_category add constraint FKgbof0jclmaf8wn2alsoexxq3u foreign key (product_id) references tb_product;
 alter table if exists tb_user_role add constraint FKea2ootw6b6bb0xt3ptl28bymv foreign key (role_id) references tb_role;
 alter table if exists tb_user_role add constraint FK7vn3h53d0tqdimm8cp45gc0kl foreign key (user_id) references tb_user;
+
